11e02b8e6a2d4bbbbe412fe261b80a64d8e73dfa
SriramTummala <sriramtummala87@gmail.com>
2020-10-27 14:07:52 -0500

    chain of resp

---
+++ src/ChainOfResponsibilityPattern/Currency.java
@@ -0,0 +1,15 @@
+package ChainOfResponsibilityPattern;
+
+public class Currency {
+
+    private int amount;
+
+    public Currency(int amt){
+        this.amount=amt;
+    }
+
+    public int getAmount(){
+        return this.amount;
+    }
+
+}
---
+++ src/ChainOfResponsibilityPattern/DispenseChain.java
@@ -0,0 +1,9 @@
+package ChainOfResponsibilityPattern;
+
+public interface DispenseChain {
+
+    void setNextChain(DispenseChain nextChain);
+
+    void dispense(Currency cur);
+
+}
